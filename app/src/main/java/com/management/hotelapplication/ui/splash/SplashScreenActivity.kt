ae4768d835595c5b70a891101a93ca67c93c1e6f
naveen.s <example@gmail.com>
2023-03-08 00:53:06 +0530

    Icons added

---
+++ app/src/main/java/com/management/hotelapplication/ui/splash/SplashScreenActivity.kt
@@ -0,0 +1,33 @@
+package com.management.hotelapplication.ui.splash
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.management.hotelapplication.databinding.ActivitySplashBinding
+import com.management.hotelapplication.ui.menuitems.MenuAddItemsActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        lifecycleScope.launch(Dispatchers.Main) {
+            delay(2000L)
+            navigateToHome()
+        }
+    }
+
+    fun navigateToHome() {
+        val intent = Intent(this, MenuAddItemsActivity::class.java)
+        startActivity(intent)
+    }
+
+}
