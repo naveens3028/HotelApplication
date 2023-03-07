781311603bcbf7b90cc761f74599773362bfd20c
naveen.s <example@gmail.com>
2023-03-08 00:22:40 +0530

    Icons added

--- app/src/main/java/com/management/hotelapplication/model/DataModel.kt
+++ app/src/main/java/com/management/hotelapplication/model/DataModel.kt
@@ -1,10 +1,12 @@
 package com.management.hotelapplication.model
 
+import android.os.Parcelable
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
-@kotlinx.serialization.Serializable
-class DataModel {
+@Parcelize
+class DataModel : Parcelable {
     @SerializedName("id")
     @Expose
     var id: Int? = null
