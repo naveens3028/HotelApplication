bc488ce5367bd1a445c8d496864189f5d40ffee0
naveen.s <example@gmail.com>
2023-03-10 20:31:42 +0530

    Database builder removed

--- app/src/main/java/com/management/hotelapplication/ui/login/NewUserRegistration.kt
+++ app/src/main/java/com/management/hotelapplication/ui/login/NewUserRegistration.kt
@@ -4,15 +4,15 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.management.hotelapplication.database.AppDatabase
-import com.management.hotelapplication.database.DatabaseBuilder
 import com.management.hotelapplication.databinding.ActivityNewuserregBinding
 import com.management.hotelapplication.table.UserLogin
+import org.koin.android.ext.android.inject
 
 class NewUserRegistration : AppCompatActivity() {
 
     lateinit var binding: ActivityNewuserregBinding
     lateinit var newUserViewModel: NewUserViewModel
-    lateinit var dat: AppDatabase
+    val dat: AppDatabase by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +20,6 @@ class NewUserRegistration : AppCompatActivity() {
         setContentView(binding.root)
 
         newUserViewModel = ViewModelProvider(this).get(NewUserViewModel::class.java)
-        dat = DatabaseBuilder.getInstance(this)
         binding.save.setOnClickListener {
             val info = UserLogin(
                 username = binding.newusername.text.toString(),
